e6461aec27a80ae5287565adcd8d44fb4df672c2
Lian Y <lian.yao@gmx.de>
2020-03-04 10:24:09 +0100

    Bruttopreis done

--- src/BruttoNetto.java
+++ src/BruttoNetto.java
@@ -3,10 +3,14 @@ public class BruttoNetto {
         int nettoPreis = 200;
         // float floatSteuersatz = 0.05f;
         double steuersatz = 0.05;
+        double maxSteuersatz = 0.04;
         //float bruttoPreis;
         double bruttoPreis;
         bruttoPreis = nettoPreis * (1 + steuersatz);
         System.out.println("Ergebnis: " + bruttoPreis + "€");
-    return;
+        boolean zuHoch;
+        zuHoch = steuersatz > maxSteuersatz;
+        System.out.println("Zu hoch?" + zuHoch);
+        return;
     }
 }
